8c492e6cf138de329b86c4e7772dc5a7ef7741b3
unknown <sizwenqwala@gmail.com>
2022-09-06 13:18:35 +0200

    added setattribute

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
+var addItemId = 0;
 function addToCart(item) {
+     addItemId += 1
      var selectedItem = document.createElement('div');
      selectedItem.classList.add('cartImg');
+     selectedItem.setAttribute('id',addItemId);
+     var cartItems = document.getElementById('title');
+     cartItems.append(selectedItem);
 }
